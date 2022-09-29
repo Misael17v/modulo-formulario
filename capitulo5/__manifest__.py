ba9b8242a8a49ba9de4a368491ad74fc109d8924
Misael17v <114034848+Misael17v@users.noreply.github.com>
2022-09-29 10:55:22 -0500

    Add files via upload

---
+++ capitulo5/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name':"My segunda libreria",
+    'summary': "manage book easily",
+    'description':"""
+    Capitulo 5""",
+    'author': "Misael",
+    'website':"",
+    'version':'1.0.0',
+    'depends':['base'],
+    'data':[
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/library_book_categ.xml'
+    ]
+    
+}

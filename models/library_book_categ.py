9ad8e68bcf5c7027ba18a9218ca4186ebd088575
Misael17v <114034848+Misael17v@users.noreply.github.com>
2022-10-15 14:26:10 -0500

    Add files via upload

--- models/library_book_categ.py
+++ models/library_book_categ.py
@@ -2,30 +2,26 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
+
 class BookCategory(models.Model):
-    _name='libreria.libro.categoria'
-    _parent_store=True
-    _parent_name="parent_id"
-    
-    name=fields.Char('Categoria')
-    description=fields.Text('Descripcion')
-    
-    parent_id=fields.Many2one(
-        'libreria.libro.categoria',
-        string='Categorias Principal',
+    _name = 'library.book.category'
+
+    _parent_store = True
+    _parent_name = "parent_id"  # optional if field is 'parent_id'
+
+    name = fields.Char('Categoria')
+    parent_id = fields.Many2one(
+        'library.book.category',
+        string='Principal Category',
         ondelete='restrict',
         index=True
     )
-    
     child_ids = fields.One2many(
-        'libreria.libro.categoria', 'parent_id',
-        string='Categorias Secundario')
+        'library.book.category', 'parent_id',
+        string='Secundaria Categoias')
     parent_path = fields.Char(index=True)
 
     @api.constrains('parent_id')
     def _check_hierarchy(self):
         if not self._check_recursion():
             raise models.ValidationError('Error! You cannot create recursive categories.')
-    
-   
-    
